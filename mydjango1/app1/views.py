cc1cd5f793b8b64d19733d8312e5f20d7440b3d0
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-08-28 20:57:33 +0400

    recipes

--- mydjango1/app1/views.py
+++ mydjango1/app1/views.py
@@ -25,6 +25,38 @@ def time(request):
 
 def workdir(request):
     dirs = f'{os.listdir()}'
-
     return HttpResponse(dirs)
 
+
+DATA = {'omlet': {'яйца, шт': 2, 'молоко, л': 0.1, 'соль, ч.л.': 0.5, },
+        'pasta': {'макароны, г': 0.3, 'сыр, г': 0.05, },
+        'buter': {'хлеб, ломтик': 1, 'колбаса, ломтик': 1, 'сыр, ломтик': 1,
+                  'помидор, ломтик': 1, }, }
+
+
+def omlet(request):
+    templates_name = 'app1/index.html'
+
+    recipes = DATA.get('omlet')
+    context = {
+        'recipe': recipes
+    }
+    servings = int(request.GET['servings'])
+
+    return render(request, templates_name, context, {servings})
+
+def pasta(request):
+    templates_name = 'app1/index.html'
+    recipes = DATA.get('pasta')
+    context = {
+        'recipe': recipes
+    }
+    return render(request, templates_name, context)
+
+def buter(request):
+    templates_name = 'app1/index.html'
+    recipes = DATA.get('buter')
+    context = {
+        'recipe': recipes
+    }
+    return render(request, templates_name, context)
